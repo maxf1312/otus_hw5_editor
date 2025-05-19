27c098c0f32eaabacaa9109b92ab0e39b2bb3de0
MaxF <m.fukalov@frendit.ru>
2025-05-19 09:05:12 +0700

    homework description to readme

--- editor.h
+++ editor.h
@@ -4,9 +4,13 @@
 
 namespace otus_hw5{
 
-struct IDocument{};
-struct IShape{};
-struct IDocView{};
+    struct IDisplay;
+    struct IDocument;
+    struct IShape;
+    struct IDocView;
 
+    struct IDocument{};
+    struct IShape{};
+    struct IDocView{};
 } // otus_hw5
 
